6257b3e7611f352919315f205ad0efbac19c6321
Amimaro <amir.zahlan@gmail.com>
2018-02-17 09:02:46 -0300

    Remove unused auth

--- server/model/user/schema.js
+++ server/model/user/schema.js
@@ -3,34 +3,11 @@ const bcrypt   = require('bcrypt-nodejs');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  local: {
-    email: String,
-    password: String
-  },
-  facebook: {
-    id: String,
-    token: String,
-    name: String,
-    email: String
-  },
   twitter: {
     id: String,
     token: String,
     displayName: String,
     username: String
-  },
-  google: {
-    id: String,
-    token: String,
-    email: String,
-    name: String
-  },
-  github: {
-    id: String,
-    username: String,
-    displayName: String,
-    email: String,
-    publicRepos: String
   },
 });
 
